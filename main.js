b86d45cef741ea8b1ea6dbf111d00adecde4cddf
joseshiang <joseshiang@gmail.com>
2019-06-03 14:07:06 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,43 @@
+//引入内置模块
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+//保持一个全局引用的窗口对象，如果不这么做，窗口将会被js垃圾回收，导致窗口关闭
+let win;
+
+//创建窗口
+function createWindow () {
+    // 创建一个浏览器窗口
+    win = new BrowserWindow({width: 800, height: 600});
+
+    // 将index.html加载到app中
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // 打开开发者工具
+    //win.webContents.openDevTools();
+
+    // 监听关闭窗口事件
+    win.on('closed', () => {
+        win = null
+    })
+}
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+    if (win === null) {
+        console.log("hi11");
+        createWindow();
+    }
+});
+
+// 所有窗口关闭后推出
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
