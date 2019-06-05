13cb08d7892ad6be94bbfd2c9d03fd2fc2fa4636
joseshiang <joseshiang@gmail.com>
2019-06-05 10:31:11 +0800

    20190301102436

--- README.md
+++ README.md
@@ -5,4 +5,15 @@
 - 渲染进程
     - electron使用chromium展示页面，chromium的多进程结构被充分利用，每个electron的页面都在运行自己的进程，这样的进程称作渲染进行
 - 一般浏览器中，网页通常使用的沙盒环境sandbox下运行，并且不允许访问原生资源，然而Electron拥有在网页调用io.js的API能力，可以与底层操作系统进行直接交互
+- 一个Electron应用只有一个主进程
+- Electron使用Chromium来展示web页面，所有chromium的多进程架构也被充分的使用，每个Electron中的web页面运行在他自己的渲染进程中
+- 使用Electron的用户在Node.js的API支持下可以在页面中与操作系统进行交互
+- 主进程使用BrowserWindow实例创建一个页面，每个BrowserWindow实例都在自己页面的渲染进程里运行页面，当BrowserWindow实例被销毁时，相应的渲染进程也会被终止
+- 主进程管理所有的渲染进程，每个渲染进程都是独立的，它只关系自己所运行的web页面
+- web页面中如何调用原生的GUI API？
+    - 由于web页面操作原生GUI资源非常危险，容易造成资源泄露，使用渲染进程与主进程进行通讯，从而调用原生GUI API
+    - 主进程 main process
+    - 渲染进程 renderer processes
+
+
 
--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const path = require('path');
 const url = require('url');
 
 //保持一个全局引用的窗口对象，如果不这么做，窗口将会被js垃圾回收，导致窗口关闭
-let win;
+let win1;
+let win2;
 
 //创建窗口
-function createWindow () {
+function createWindow1 () {
     // 创建一个浏览器窗口
-    win = new BrowserWindow({
+    win1 = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -18,19 +19,43 @@ function createWindow () {
     });
 
     // 将index.html文件
-    win.loadFile("index.html");
+    win1.loadFile("index.html");
 
     // 打开开发者工具
     //win.webContents.openDevTools();
 
     // 监听关闭窗口事件,当window窗口被关闭时候，这个事件会出发
-    win.on('closed', () => {
-        win = null
+    win1.on('closed', () => {
+        win1 = null
+    })
+}
+
+//创建窗口
+function createWindow2 () {
+    // 创建一个浏览器窗口
+    win2 = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // 将index.html文件
+    win2.loadFile("index.html");
+
+    // 打开开发者工具
+    //win.webContents.openDevTools();
+
+    // 监听关闭窗口事件,当window窗口被关闭时候，这个事件会出发
+    win2.on('closed', () => {
+        win1 = null
     })
 }
 
 //创建浏览器窗口调用这个函数，部分API在调用ready事件触发才能使用
-app.on('ready', createWindow);
+app.on('ready', createWindow1);
+app.on('ready', createWindow2);
 
 // 所有窗口关闭后退出
 app.on('window-all-closed', () => {
